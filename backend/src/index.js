eddb51ca629b48061f8c6fe2ecba0c77cfc7396a
Nitish Kumar <163245178+nitish-kumar3@users.noreply.github.com>
2026-05-11 22:34:13 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,7 +3,6 @@ const express = require('express');
 const cors = require('cors');
 const { testConnection } = require('./config/db');
 
-
 const app = express();
 
 app.use(cors({ origin: process.env.FRONTEND_URL || 'http://localhost:3000', credentials: true }));
